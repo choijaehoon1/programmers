426ae34ec5d9bb0ead81c666d1c426ea16ec050c
Jae Hoon Choi <37289182+choijaehoon1@users.noreply.github.com>
2021-01-10 23:36:41 +0900

    Update TravelRoute.java

--- programmers/src/main/java/com/sist/ehr/dfsbfs/TravelRoute.java
+++ programmers/src/main/java/com/sist/ehr/dfsbfs/TravelRoute.java
@@ -1,57 +1,32 @@
-package com.sist.ehr.dfsbfs;
+//여행경로
 import java.util.*;
-
-public class TravelRoute {
-	static List<String> list = new ArrayList<String>();
-	static String route = "";
-	static boolean[] visit;
-	
-	public static void dfs(String[][] tickets, String end, int cnt) {
-		route += end + ",";
-		
-		if(cnt == tickets.length) {
-			list.add(route); 
-            return;
-		}
-		
-		for(int i = 0; i < tickets.length; i++) {
-			String s = tickets[i][0];
-            String e = tickets[i][1];
-			if(s.equals(end) && !visit[i]) {	//visit[i]가 false일때 조건문 탐
-				visit[i] = true;//들렸으면 true로 표시
-				dfs(tickets, e, cnt + 1);
-				visit[i] = false;//또다른 경로 있나 찾으려고
-                route = route.substring(0, route.length()-4);
-			}
-		}
-	}
-	
-	public static String[] solution(String[][] tickets) {
-		for(int i = 0; i < tickets.length; i++) {
-			visit = new boolean[tickets.length];
-			String start = tickets[i][0];
-			String end = tickets[i][1];
-			
-			if(start.equals("ICN")) {
-				route = start + ","; 
-                visit[i] = true; 
-				dfs(tickets, end, 1);
-			}
-		}
-		
-		Collections.sort(list);
-		String[] answer = list.get(0).split(",");
-		for(int i=0; i<answer.length;i++) {
-			System.out.println(answer[i]);
-		}
-		
-		return answer;
+class Solution {
+    ArrayList<String> list = new ArrayList<>();
+    
+    public void dfs(String[][] tickets, String route, String t ,int cnt ,int[] visit){
+        t += route + ',';
+        if ( cnt == tickets.length){
+            list.add(t);
+        }
+        else{
+            for(int i=0;i<tickets.length;i++){
+                if(route.equals(tickets[i][0]) && visit[i] == 0){
+                    visit[i] = 1;
+                    dfs(tickets,tickets[i][1],t,cnt+1,visit);
+                    visit[i] = 0;
+                }
+            }
+        }
+        
     }
-	
-	//ICN, SFO], [ICN, ATL], [SFO, ATL], [ATL, ICN], [ATL,SFO]]
-	public static void main(String[] args) {
-		String tickets[][] = {{"ICN", "SFO"}, {"ICN", "ATL"}, {"SFO", "ATL"}
-							, {"ATL", "ICN"}, {"ATL", "SFO"}}; 
-		System.out.println(solution(tickets));
-	}
+    
+    public String[] solution(String[][] tickets) {
+        int[] visit = new int[tickets.length];
+        dfs(tickets,"ICN","",0,visit);
+        Collections.sort(list);
+        // System.out.println(list.toString());
+        String[] answer = list.get(0).split(",");
+            
+        return answer;
+    }
 }
